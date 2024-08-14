0d79b8ed350e72889ea4c737d304af2652a6270f
Arthur Duvareski <147620655+ArthurDRF@users.noreply.github.com>
2024-08-13 23:29:13 -0300

    Inicio da lista de exercicios
    Criação da Main e Classes "Churrasco" e "Pessoa" referentes ao exercício 1

---
+++ Lista/com/lista/Pessoa.java
@@ -0,0 +1,19 @@
+package com.lista;
+
+/*Exercício 1. 
+Classe Pessoa que possui os atributos: String nome, String sexo, int idade, boolean vegetariana. (Feito)
+Faca agora uma classe Churrasco que possua: Atributos: qtdCarne(double); Metodo: verificarConsumo(): (Feito)
+
+Recebe via parametro uma Pessoa e com isto define a consumacao media de carne (quantidade de carne consumida) 
+
+Pessoas de 0 a 3 anos nao consomem, vegetarianos tambem nao. 
+Pessoas de 4 a 12 anos consomem 1 kilo de carne
+13 anos em diante 2 kilos de carne. */
+
+public class Pessoa{
+
+    String nome, sexo;
+    int idade;
+    boolean vegetariana;
+    
+}
